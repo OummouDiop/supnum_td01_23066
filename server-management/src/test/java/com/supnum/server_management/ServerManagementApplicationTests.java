260f8d52a631f3ff02b4ae9bdc2c6552622a5c16
OummouDiop <23066@supnum.mr>
2025-11-21 16:19:24 +0000

    Ajout des dépendances (Web, JPA, MySQL) + configuration MySQL

---
+++ server-management/src/test/java/com/supnum/server_management/ServerManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supnum.server_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
